f1c53602fbf4e077027db805a335eb45d081a16f
MakiZushi <aoibotowner@gmail.com>
2025-06-11 20:26:36 +0900

    feat(project): Add api handle

--- src/main/kotlin/com/github/aoideveloper/simpleEconomy/SimpleEconomy.kt
+++ src/main/kotlin/com/github/aoideveloper/simpleEconomy/SimpleEconomy.kt
@@ -1,18 +1,37 @@
 package com.github.aoideveloper.simpleEconomy
 
 import com.github.aoideveloper.simpleEconomy.api.EconomyServiceImpl
+import com.github.aoideveloper.simpleEconomy.database.DatabaseManager
+import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
 
 class SimpleEconomy : JavaPlugin() {
+    private lateinit var databaseManager: DatabaseManager
+
     companion object {
-        val instance = EconomyServiceImpl()
+        lateinit var service: EconomyServiceImpl
+        lateinit var plugin: Plugin
     }
 
     override fun onEnable() {
-        // Plugin startup logic
+        plugin = this
+
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs()
+        }
+
+        databaseManager = DatabaseManager(dataFolder)
+        databaseManager.connect()
+
+        service = EconomyServiceImpl()
+        service.loadAllBalances()
+
+        logger.info("SimpleEconomy has been enabled.")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        service.saveAllBalances()
+
+        logger.info("SimpleEconomy has been disabled.")
     }
 }
