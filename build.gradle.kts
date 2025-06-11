da5fbaec4847e8c2f5e151474f6ee15fbe4c6c54
MakiZushi <aoibotowner@gmail.com>
2025-06-11 21:12:09 +0900

    chore(project): Merge META-INF/services/ on build

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,15 +65,29 @@ kotlin {
     jvmToolchain(targetJavaVersion)
 }
 
-tasks.build {
-    dependsOn("shadowJar")
-}
+tasks {
+    shadowJar {
+        relocate("org.jetbrains.exposed", "com.github.aoideveloper.simpleEconomy.libs.exposed")
+        relocate("org.xerial.sqlite", "com.github.aoideveloper.simpleEconomy.libs.sqlite")
 
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
-        expand(props)
+        archiveBaseName.set(rootProject.name)
+        archiveClassifier.set("")
+        archiveVersion.set(project.version.toString())
+
+        mergeServiceFiles()
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
     }
 }
+
